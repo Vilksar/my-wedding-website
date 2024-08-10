360280bf8c2e4f711097a0fcc0d885f9e9f718d5
Victor Popescu <19798302+Vilksar@users.noreply.github.com>
2024-08-10 13:56:33 +0300

    Added support for English, Romanian, and Ukrainian languages.

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,5 +13,16 @@ export const collections = {
             icon: image(),
             index: z.number()
         })
+    }),
+    "languages": defineCollection({
+        type: "data",
+        schema: ({ image }) => z.object({
+            id: z.string(),
+            emoji: z.string(),
+            icon: image(),
+            direction: z.union([z.literal("ltr"), z.literal("rtl")]),
+            nativeName: z.string(),
+            translations: z.record(z.string(), z.string()),
+        })
     })
 };
--- src/scripts/server.ts
+++ src/scripts/server.ts
@@ -25,7 +25,12 @@ const content = {
         .filter(entry => ensureEntryId(entry))
         .map(entry => entry.data)
         .toSorted((data1, data2) => data1.index - data2.index)
-        .reduce((accumulator, data) => ({ ...accumulator, [data.id]: data }), {} as { [key: string]: InferEntrySchema<"colorSchemes"> })
+        .reduce((accumulator, data) => ({ ...accumulator, [data.id]: data }), {} as { [key: string]: InferEntrySchema<"colorSchemes"> }),
+    languages: (await getCollection("languages"))
+        .filter(entry => ensureEntryId(entry))
+        .map(entry => entry.data)
+        .toSorted((data1, data2) => data1.id.localeCompare(data2.id))
+        .reduce((accumulator, data) => ({ ...accumulator, [data.id]: data }), {} as { [key: string]: InferEntrySchema<"languages"> }),
 };
 
 /**
@@ -45,10 +50,81 @@ export function getColorScheme(colorSchemeId: string): InferEntrySchema<"colorSc
 }
 
 /**
+ * Gets a language.
+ * @returns The language.
+ */
+export function getLanguage(languageId: string): InferEntrySchema<"languages"> {
+    // Get the language.
+    const language = content.languages[languageId];
+    // Check if there is no language.
+    if (language === null || language === undefined) {
+        // Throw an error.
+        throw new Error(`A language with the ID "${languageId}" does not exist.`);
+    }
+    // Return the language.
+    return language;
+}
+
+/**
  * Gets the color schemes.
  * @returns The color schemes.
  */
 export function getColorSchemes(): InferEntrySchema<"colorSchemes">[] {
     // Return the color schemes.
     return Object.values(content.colorSchemes);
+}
+
+/**
+ * Gets the languages.
+ * @returns The languages.
+ */
+export function getLanguages(): InferEntrySchema<"languages">[] {
+    // Return the color schemes.
+    return Object.values(content.languages);
+}
+
+/**
+ * Gets a translation.
+ * @param languageId The language ID.
+ * @param translationId The translation ID.
+ * @param parameters The parameters.
+ * @returns The translation.
+ */
+export function getTranslation(languageId: string, translationId: string, parameters?: string[]): string {
+    // Get the translation.
+    const translation = getLanguage(languageId).translations?.[translationId];
+    // Check if there is no translation.
+    if (translation === null || translation === undefined) {
+        // Throw an error.
+        throw new Error(`The translation with ID "${translationId}" for language with ID "${languageId}" does not exist.`);
+    }
+    // Return the translation.
+    return parameters?.reduce((accumulator, parameter, index) => accumulator.replaceAll(`{${index}}`, parameter), translation) ?? translation;
+}
+
+/**
+ * Gets the localized route for an URL, by replacing the language ID if it exists, or adding it if not.
+ * @param url The URL.
+ * @param oldLanguageId The old language ID.
+ * @param newLanguageId The new language ID.
+ * @returns The localized route.
+ */
+export function getLocalizedRoute(url: string, oldLanguageId: (string | undefined), newLanguageId: (string | undefined)): string {
+    // Check if there is no old language ID and no new language ID.
+    if ((oldLanguageId === null || oldLanguageId === undefined) && (newLanguageId === null || newLanguageId === undefined)) {
+        // Return the URL.
+        return url;
+    }
+    // Check if there is no old language ID.
+    if (oldLanguageId === null || oldLanguageId === undefined) {
+        // Return the URL with the language ID added.
+        return url.replace(`/`, `/${newLanguageId}/`);
+    }
+    // Check if there is no new language ID.
+    if (newLanguageId === null || newLanguageId === undefined) {
+        // Return the URL with the language ID removed.
+        return url.replace(`/${oldLanguageId}/`, `/`);
+    }
+    // Return the URL with the language ID replaced.
+    return url.replace(`/${oldLanguageId}/`, `/${newLanguageId}/`);
 }
